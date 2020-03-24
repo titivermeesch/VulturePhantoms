22c1d856d4d604137dfe1d381e8f7def21086ad4
Tristan Vermeesch <titivermeesch@gmail.com>
2020-03-24 17:43:01 +0100

    add creative kill prevention

--- src/main/java/com/playbosswar/vulturephantoms/Main.java
+++ src/main/java/com/playbosswar/vulturephantoms/Main.java
@@ -3,6 +3,7 @@ package com.playbosswar.vulturephantoms;
 import com.playbosswar.vulturephantoms.commands.VultureCommand;
 import com.playbosswar.vulturephantoms.listeners.PlayerDeath;
 import com.playbosswar.vulturephantoms.listeners.TakeFire;
+import com.playbosswar.vulturephantoms.listeners.VultureDamage;
 import com.playbosswar.vulturephantoms.listeners.VultureDeath;
 import com.playbosswar.vulturephantoms.utilities.PhantomDespawner;
 import org.bukkit.Bukkit;
@@ -21,6 +22,7 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
         getServer().getPluginManager().registerEvents(new TakeFire(), this);
         getServer().getPluginManager().registerEvents(new VultureDeath(), this);
+        getServer().getPluginManager().registerEvents(new VultureDamage(), this);
         Objects.requireNonNull(getCommand("vulturephantoms")).setExecutor(new VultureCommand());
         Bukkit.getConsoleSender().sendMessage("VulturePhantoms v1.1 has been enabled");
     }
---
+++ src/main/java/com/playbosswar/vulturephantoms/listeners/VultureDamage.java
@@ -0,0 +1,27 @@
+package com.playbosswar.vulturephantoms.listeners;
+
+import com.playbosswar.vulturephantoms.Main;
+import com.playbosswar.vulturephantoms.VultureManager;
+import com.playbosswar.vulturephantoms.utilities.Vulture;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Phantom;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.Plugin;
+
+public class VultureDamage implements Listener {
+    private static Plugin plugin = Main.getPlugin();
+
+    @EventHandler
+    public void onVultureDamage(EntityDamageByEntityEvent e) {
+        Player p = (Player) e.getDamager();
+        Vulture vulture = VultureManager.getVulture((Phantom) e.getEntity());
+        assert vulture != null;
+
+        if(plugin.getConfig().getBoolean("preventCreativeKill") && p.getGameMode() == GameMode.CREATIVE) {
+            e.setCancelled(true);
+        }
+    }
+}
